51292c5153d6566b9909b4b6ca75e5a629b1bda7
Jorge R <jorgerodriguez434@gmail.com>
2018-03-01 19:50:07 -0600

    init commit -j

--- models.js
+++ models.js
@@ -1,4 +1,33 @@
-{
+const uuid = require('uuid');
+
+// This module provides volatile storage, using a `BlogPost`
+// model. We haven't learned about databases yet, so for now
+// we're using in-memory storage. This means each time the app stops, our storage
+// gets erased.
+
+// Don't worry too much about how BlogPost is implemented.
+// Our concern in this example is with how the API layer
+// is implemented, and getting it to use an existing model.
+
+
+function StorageException(message) {
+   this.message = message;
+   this.name = "StorageException";
+}
+
+const BlogPosts = {
+  create: function(title, content, author, publishDate) {
+    const post = {
+      id: uuid.v4(),
+      title: title,
+      content: content,
+      author: author,
+      publishDate: publishDate || Date.now()
+    };
+    this.posts.push(post);
+    return post;
+  },
+  get: function(id=null) {
     // if id passed in, retrieve single post,
     // otherwise send all posts.
     if (id !== null) {
