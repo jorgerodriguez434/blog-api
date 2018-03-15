89d5566d5b6dddbac546051412aafbca4f757d23
Jorge R <jorgerodriguez434@gmail.com>
2018-03-14 21:15:21 -0500

    fail with mongoose

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -1,38 +1,46 @@
+'use strict';
 
 const express = require('express');
 const router = express.Router();
 
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise; //
 
 const {BlgPosts} = require('./models'); 
 
+/*
 BlgPosts.create('The Forgotten Road','Fiction','Richard Paul Evans');
 BlgPosts.create('Baker\'s secret', 'Fiction','Stephen P. Kiernan');
 BlgPosts.create('Clive Cussler','Fiction','Graham Brown');
+*/
 
 router.get('/', (req, res) => {
 
 	console.log('making a GET request -jr');
-	res.json(BlgPosts.get());
-	res.status(200).json();
+	BlgPosts.find()
+
+});
+
+router.get('/:id', (req, res) => {
+
+	console.log('making a GET request by ID -jr');
+	//BlgPosts.findById(req.params.id);
 
 });
 
 router.post('/', jsonParser, (req, res) => {
 
 	console.log('making a POST request -jr');
-	const blog = BlgPosts.create(req.body.title, req.body.content, req.body.author);
-	res.status(201).json(blog); // `blog as argument`
+	//BlgPosts.create({title: req.body.title, content: req.body.content, author: req.body.author });
 
 });
 
 router.delete('/:id', (req, res) => {
 
 	console.log('making a DELETE request -jr');
-	const targetBlogID = req.params.id
-	console.log(`deleting blog post: ${targetBlogID}`);
-	BlgPosts.delete(targetBlogID);
+	//BlgPosts.findByIdAndRemove(req.params.id)
 
 	res.status(204).end();
 
@@ -40,6 +48,7 @@ router.delete('/:id', (req, res) => {
 
 router.put('/:id', (req, res) => {
 
+	console.log('making a PUT request -jr');
 	res.status(204).end();
 
 });
