2364f655eec323a523349df0b16de2c5078e99ac
Jorge R <jorgerodriguez434@gmail.com>
2018-03-14 21:37:07 -0500

    try to build

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -1,46 +1,38 @@
-'use strict';
 
 const express = require('express');
 const router = express.Router();
 
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise; //
 
-const {BlgPosts} = require('./models'); 
-
-/*
+const {BlgPosts} = require('./models'); //this means, that BlgPosts will get all the functions of the model, that is all I know,
+										// has to be the same name as in from the model.js
 BlgPosts.create('The Forgotten Road','Fiction','Richard Paul Evans');
 BlgPosts.create('Baker\'s secret', 'Fiction','Stephen P. Kiernan');
 BlgPosts.create('Clive Cussler','Fiction','Graham Brown');
-*/
 
 router.get('/', (req, res) => {
 
-	console.log('making a GET request -jr');
-	BlgPosts.find()
-
-});
-
-router.get('/:id', (req, res) => {
-
-	console.log('making a GET request by ID -jr');
-	//BlgPosts.findById(req.params.id);
+	/*console.log('making a GET request -jr');
+	res.json(BlgPosts.get());*/
+	res.status(200).json();
 
 });
 
 router.post('/', jsonParser, (req, res) => {
 
-	console.log('making a POST request -jr');
-	//BlgPosts.create({title: req.body.title, content: req.body.content, author: req.body.author });
+	/*console.log('making a POST request -jr');
+	const blog = BlgPosts.create(req.body.title, req.body.content, req.body.author);*/
+	res.status(201).json(); // `blog as argument`
 
 });
 
 router.delete('/:id', (req, res) => {
 
-	console.log('making a DELETE request -jr');
-	//BlgPosts.findByIdAndRemove(req.params.id)
+	/*console.log('making a DELETE request -jr');
+	const targetBlogID = req.params.id
+	console.log(`deleting blog post: ${targetBlogID}`);
+	BlgPosts.delete(targetBlogID);*/
 
 	res.status(204).end();
 
@@ -48,7 +40,6 @@ router.delete('/:id', (req, res) => {
 
 router.put('/:id', (req, res) => {
 
-	console.log('making a PUT request -jr');
 	res.status(204).end();
 
 });
--- config.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://jp:jppw@ds213239.mlab.com:13239/my-first-mlab-db';
-exports.PORT = process.env.PORT || 8080;
